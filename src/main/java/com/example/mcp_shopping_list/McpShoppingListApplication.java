3578ac2f82e9e96c793b2158822b2bea4dda002f
Ruize Zhang <ruizezhang@192.168.5.50>
2025-08-04 22:17:56 +0800

    init

---
+++ src/main/java/com/example/mcp_shopping_list/McpShoppingListApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcp_shopping_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpShoppingListApplication.class, args);
+	}
+
+}
